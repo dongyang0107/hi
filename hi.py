6244f7b85d6382097db0b07e553eece4257c0cca
chendongyang <110306024@nccu.edu.tw>
2023-06-21 16:19:01 +0800

    Add new file

---
+++ hi.py
@@ -0,0 +1,9 @@
+# 把輸入的東西存到name
+# name = input('請輸入名字： ')
+# print('嗨', name)\
+
+height = input('請問你的身高是多少呢： ')
+print('原來你的身高是', height, "公分啊")
+weight = input('那請問你的體重是多少呢：')
+print('你的體重是', weight, '公斤啊')
+print('謝謝你回答所有的問題')
